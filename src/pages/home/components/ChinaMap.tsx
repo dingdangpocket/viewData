690ec422940d866839e21fe484632db244faa8cc
niegang <1003018791@qq.com>
2022-05-22 16:59:55 +0800

    map

--- src/pages/home/components/ChinaMap.tsx
+++ src/pages/home/components/ChinaMap.tsx
@@ -2,9 +2,10 @@ import axios from 'axios';
 import * as echarts from 'echarts/core';
 import { useEffect, useRef, useState } from 'react';
 import { GeoComponent } from 'echarts/components';
+import { MapChart } from 'echarts/charts';
 import Styles from '../home.module.less';
 import chinaMap from '../../china.json';
-echarts.use([GeoComponent]);
+echarts.use([GeoComponent, MapChart]);
 export default function ChinaMap() {
   const chartRef = useRef<HTMLInputElement>(null);
   const [charts, setCharts] = useState<any>();
@@ -17,6 +18,40 @@ export default function ChinaMap() {
       geo: {
         map: 'china',
         zoom: 1.2,
+        itemStyle: {
+          areaColor: '#e8e8e8',
+        },
+        label: { show: true },
+        emphasis: {
+          itemStyle: {
+            areaColor: '#47a781',
+          },
+        },
+      },
+      tooltip: {
+        show: true,
+      },
+      series: {
+        type: 'map',
+        geoIndex: 0,
+        data: [
+          { name: '山东', value: 0 },
+          { name: '四川', value: 10 },
+          { name: '山西', value: 100 },
+        ],
+      },
+      visualMap: {
+        type: 'piecewise',
+        pieces: [
+          { min: 0, max: 0, label: '0', color: '#64c977' },
+          { min: 1, max: 9, label: '1-9', color: '#bdce52' },
+          { min: 10, max: 100, label: '10-100', color: '#b76d6d' },
+          { min: 100, max: 1000, label: '100-1000', color: '#e61111' },
+        ],
+        itemWidth: 10,
+        itemHeight: 10,
+        itemGap: 2,
+        inverse: false,
       },
     };
     chart.setOption(option);
